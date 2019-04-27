5cc315a8f83e90c162f6871863c0676f189bfb9a
Brandon El Haj <brandon.elhaj@gmail.com>
2019-04-27 16:16:05 -0700

    express drills final

--- app.js
+++ app.js
@@ -95,6 +95,54 @@ app.get('/lotto', (req, res) => {
    
 })
 
+
+app.get('/grade', (req, res) => {
+// get the mark from the query
+const { mark } = req.query;
+
+// do some validation
+if(!mark) {
+    // mark is required
+    return res
+    .status(400)
+    .send("Please provide a mark");
+}
+
+const numericMark = parseFloat(mark);
+if(Number.isNaN(numericMark)) {
+    // mark must be a number
+    return res
+    .status(400)
+    .send("Mark must be a numeric value");
+}
+
+if(numericMark < 0 || numericMark > 100) {
+    // mark must be in range 0 to 100
+    return res
+    .status(400)
+    .send("Mark must be in range 0 to 100");
+}
+
+if(numericMark >= 90) {
+    return res
+    .send("A");
+} 
+
+if(numericMark > 80) {
+    return res
+    .send("B");
+}
+
+if(numericMark >= 70) {
+    return res
+    .send("C");
+}
+
+res
+    .send("F");
+});
+
+
 app.listen(8000, () => {
     console.log('The port 8000 is up and running mofo!');
 }); 
